bdaf8f18933b2990b5bd5bd710c2787862ff337e
sachetwasti1997 <sachetwasti61@gmail.com>
2022-06-28 21:39:48 +0530

    Code push for graph related problems

--- src/graph_adv/AlienDict.kt
+++ src/graph_adv/AlienDict.kt
@@ -1,18 +1,11 @@
 package graph_adv
 
 class AlienDict {
-    data class Node(val chr: Char?, var length: Int = 1, var next: Node? = null)
     fun alienOrder(words: Array<String>): String {
-        if(words.size == 1){
-            var res = ""; val set = HashSet<Char>()
-            for(i in 0 until words[0].length - 1)
-                if(!set.contains(words[0][i]))res += !set.contains(words[0][i])
-            return res
-        }
-        val charMap = HashMap<Char, Node>()
+        val charMap = HashMap<Char, ArrayList<Char>>()
         for(word in words){
-            for(i in word.indices){
-                if(charMap[word[i]] == null)charMap[word[i]] = Node(word[i])
+            for(i in word){
+                charMap[i] = ArrayList()
             }
         }
         for(i in 0 until words.size - 1){
@@ -20,34 +13,36 @@ class AlienDict {
             for(k in w1.indices){
                 if(k == w2.length)return ""
                 if(k < w2.length && w1[k] != w2[k]){
-                    charMap[w1[k]]?.next = charMap[w2[k]]
-                    val l1 = charMap[w1[k]]?.length ?: 0
-                    val l2 = charMap[w2[k]]?.length ?: 0
-                    charMap[w1[k]]?.length = l1 + l2
+                    charMap[w1[k]]?.add(w2[k])
                     break
                 }
             }
         }
-        var list = charMap[words[0][0]]
-        val set = HashSet<Char?>()
+        val visited = HashSet<Char>()
+        val beingVisited = HashSet<Char>()
         var res = ""
-        while(list != null){
-            if(!set.contains(list.chr)){
-                res += list.chr
-                set.add(list.chr)
-            }else{
-                res = ""
-                break
+        fun dfs(c: Char): Boolean{
+            beingVisited.add(c)
+            visited.add(c)
+            for(i in charMap[c] ?: ArrayList()){
+                if(!visited.contains(i) && dfs(i)) return true
+                else if(beingVisited.contains(i))return true
             }
-            list = list.next
+            beingVisited.remove(c)
+            res += c
+            return false
         }
-        println(charMap)
-        return res
+        for(i in charMap){
+            if(!visited.contains(i.key)){
+                if(dfs(i.key)) return ""
+            }
+        }
+        return res.reversed()
     }
 }
 
 fun main(){
     val a = AlienDict()
-    val words = arrayOf("wrt","wrf","er","ett","rftt")
+    val words = arrayOf("z","z")
     println(a.alienOrder(words))
 }
